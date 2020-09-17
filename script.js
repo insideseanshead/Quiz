15b4bc2623e1e68cc259f876a4114cea8edcf69c
Sean Morgan <insideseanshead@gmail.com>
2020-09-16 20:58:53 -0700

    updated script.js with working timer and local storage.

--- script.js
+++ script.js
@@ -2,6 +2,25 @@
 var startPage = document.querySelector('.startScreen')
 var quizPage = document.querySelector('.quizPage')
 var scorePage = document.querySelector('.scorePage')
+var gameOver = document.querySelector('.gameOver')
+
+
+var countDown = document.querySelector('.countDown')
+
+// log high scores
+
+var score = localStorage.getItem('scores');
+
+if(score){
+  score = JSON.parse(score);
+} else {
+  score = [];
+}
+
+
+
+
+
 
 // Make variables that I will call uppon.
 var questionText = document.querySelector("#theQuestion");
@@ -25,59 +44,81 @@ var fourImg = document.querySelector("#imgFour");
 var fourText = document.querySelector("#pFour");
 var fourTrue = " ";
 
+// Game Timer
+var timeLeft = 31;
+
+function setTimer() {
+  var timeInt = setInterval(function() {
+    timeLeft--;
+    countDown.textContent = "Time Remaining " + timeLeft;
+
+    if(timeLeft === 0){
+      clearInterval;
+      quizPage.style.display = "none";
+      scorePage.style.display = "block";
+    }
+
+  }, 1000)
+}
+
 // create an array of objects which consists of questions and answers.
 
 var theBatman = [
   {
     question:"In Batman the Animated Series who was the original voice of the joker?",
-    // correctIndex: 2,
+    correctAnswerId: "answerThree",
     answerOneImg: "images/MarkHammil.jpg",
     answerOneText: "Mark Hammil",
-    oneCorrect: false,
     answerTwoImg: "images/HankAzaria.jpg",
     answerTwoText: "Hank Azaria",
-    twoCorrect: false,
     answerThreeImg: "images/TimCurry.jpg",
     answerThreeText: "Tim Curry",
-    threeCorrect: true,
     answerFourImg: "images/TonyShaloub.jpg",
     answerFourText: "Tony Shaloub",
-    fourCorrect: false,
   },
   {
     question: "Who has not been the voice of Batman?",
+    correctAnswerId: "answerFour",
     answerOneImg: "images/KevinConroy.jpg",
     answerOneText: "Kevin Conroy",
-    oneCorrect: false,
     answerTwoImg: "images/DiedrichBader.jpg",
     answerTwoText: "Diedrich Bader",
-    twoCorrect: false,
     answerThreeImg: "images/WillFriedle.jpg",
     answerThreeText: "Will Fridele",
-    threeCorrect: false,
     answerFourImg: "images/CaseyKasem.jpg",
-    answerFourText: "Casey Kasem",
-    fourCorrect: true,
+    answerFourText: "Casey Kasem", 
   },
   {
     question: "Who is Colin Farrell playing in 'The Batman'?",
+    correctAnswerId: "answerThree",
     answerOneImg: "images/Joker.jpg",
     answerOneText: "The Joker?",
-    oneCorrect: false,
     answerTwoImg: "images/TwoFace.jpg",
     answerTwoText: "Two Face",
-    twoCorrect: false,
     answerThreeImg: "images/Penguin.jpg",
     answerThreeText: "The Penguin",
-    threeCorrect: true,
     answerFourImg: "images/CondimentKing.jpg",
     answerFourText: "Condiment King",
-    fourCorrect: false,
+    
   },
+  {
+    question: "Who has not been the voice of Batman?",
+    correctAnswerId: "answerFour",
+    answerOneImg: "images/KevinConroy.jpg",
+    answerOneText: "Kevin Conroy",
+    answerTwoImg: "images/DiedrichBader.jpg",
+    answerTwoText: "Diedrich Bader",
+    answerThreeImg: "images/WillFriedle.jpg",
+    answerThreeText: "Will Fridele",
+    answerFourImg: "images/CaseyKasem.jpg",
+    answerFourText: "Casey Kasem", 
+  }
 ];
 
 //Start Game
 quizPage.style.display = "none";
+scorePage.style.display = "none";
+gameOver.style.display = "none";
 
 function newQuestion(){
     // Put Question in Jumbotron
@@ -85,45 +126,80 @@ function newQuestion(){
     // fill first answer elements
     oneImg.src = theBatman[questionIndex].answerOneImg;
     oneText.textContent = theBatman[questionIndex].answerOneText;
-    oneTrue = theBatman[questionIndex].oneCorrect;
+
     // fill second answer elements
     twoImg.src = theBatman[questionIndex].answerTwoImg;
     twoText.textContent = theBatman[questionIndex].answerTwoText;
-    twoTrue = theBatman[questionIndex].twoCorrect;
+   
     // fill third answer elements
     threeImg.src = theBatman[questionIndex].answerThreeImg;
     threeText.textContent = theBatman[questionIndex].answerThreeText;
-    threeTrue = theBatman[questionIndex].threeCorrect;
+    
     //fill fourth answer elements
     fourImg.src = theBatman[questionIndex].answerFourImg;
     fourText.textContent = theBatman[questionIndex].answerFourText;
-    fourTrue = theBatman[questionIndex].fourCorrect;
+    
 }
 
 startBtn.addEventListener("click", function () {
     startPage.style.display = "none";
     quizPage.style.display = "block";
+    setTimer();
     newQuestion();
 });
 
 //answer buttons functions
 
-
-
-
-
 answerSelect.forEach(function(answerBtn){
   
-  answerBtn.addEventListener("click", function(){
-    console.log("yay")
-    questionIndex++
-    newQuestion()
+  
+  answerBtn.addEventListener("click", function(event){
+    var answerElement = event.target.closest(".answerSelect")
+    // if(answerElement.id === theBatman[questionIndex].correctAnswerId){
+    //   console.log('hopefully this works')
+    // }
+    
+
+
+    if(questionIndex < theBatman.length-1){
+      if(answerElement.id === theBatman[questionIndex].correctAnswerId){
+        
+      } else {
+        timeLeft -= 10;
+      }
+      console.log('hopefully this works')
+      questionIndex++
+      newQuestion()
+      
+    } else {
+      //stop and log time.
+      //close quiz page.
+      //open score page.
+      // var userScore = timeLeft;
+      
+      quizPage.style.display="none";
+      scorePage.style.display="block"
+      youWin()
+    }
   })
 
 })
 
 
+function youWin(){
+  var userScore = timeLeft;
+  var playerInfo = prompt(("Please enter your name."))
+  var playerObject = {
+    playerName: playerInfo,
+    playerScore: userScore
+  }
 
-console.log(theBatman);
+  score.push(playerObject);
+
+  localStorage.setItem('scores', JSON.stringify(score))
+
+
+}
+
 
 //
